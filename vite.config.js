5191c73e997fc889cb7722574d28dfc99848c537
Christopher Coogan <chris@thebrainchain.io>
2023-12-11 07:57:05 -0500

    linting and yarn

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: 'bci2k',
-            fileName: (format) => `bci2k.${format}.js`
-        },
-        rollupOptions: {
-            external: ['websocket'],
-            output: {
-                globals: {
-                    websocket: 'websocket'
-                },
-
-            },
-        },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "bci2k",
+      fileName: (format) => `bci2k.${format}.js`,
     },
-})
+    rollupOptions: {
+      external: ["websocket"],
+      output: {
+        globals: {
+          websocket: "websocket",
+        },
+      },
+    },
+  },
+});
